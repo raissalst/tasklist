bb2e409a9b4a44c137115b364200e388fef74fcc
Raissa Toledo <raissalst@gmail.com>
2022-04-05 18:24:01 -0300

    feat: implementing app configs in main init

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from app import routes
+from app.configs import database, migrations
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["JSON_SORT_KEYS"] = False
+
+    database.init_app(app)
+    migrations.init_app(app)
+
+    routes.init_app(app)
+
+    return app
